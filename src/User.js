3197708d5fec9f386d7d619b8ead982a6cd03a0b
monstaro <monstaro32@gmail.com>
2020-02-04 15:31:42 -0700

    Brings in user data

--- src/User.js
+++ src/User.js
@@ -1,8 +1,12 @@
 class User {
   constructor(userData) {
-
-// id, name, address, email, strideLength, dailyStepGoal, friends(with friend ids)
-    
+    this.id = userData.id
+    this.name = userData.name
+    this.address = userData.address
+    this.email = userData.email
+    this.strideLength = userData.strideLength
+    this.dailyStepGoal = userData.dailyStepGoal
+    this.friends = userData.friends
   }
 
   returnFirstName() {
