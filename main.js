43432c4ea62867e8bacc05401aceb888d2f978bf
Lili Manrique <liliana.manrique1@gmail.com>
2020-02-04 16:22:36 -0700

    Try to link files

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-const userData = require('./data/users.js');
-const User = require ('./src/User.js');
-const UserRepository = require ('./src/UserRepository.js');
-
-loadUser();
+// const userData = require('./data/users.js');
+// const User = require ('./src/User.js');
+// const UserRepository = require ('./src/UserRepository.js');
+const userSpan = document.getElementById('user-name');
 
 const loadUser = () => {
   let random = Math.floor(Math.random()*(50));
   const userRepo = new UserRepository(userData);
   const user = new User(random);
+  console.log(user.returnFirstName())
+  userSpan.innerText = user.returnFirstName();
 }
+
+loadUser();
