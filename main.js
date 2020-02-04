967d0e522aa2a36efb3ea4764104ac6c6a1e4a6a
Lili Manrique <liliana.manrique1@gmail.com>
2020-02-04 16:24:58 -0700

    Add comment

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const userSpan = document.getElementById('user-name');
 
 const loadUser = () => {
   let random = Math.floor(Math.random()*(50));
+//We are getting a 'UserRepository is not defined' error on the line below
   const userRepo = new UserRepository(userData);
   const user = new User(random);
   console.log(user.returnFirstName())
