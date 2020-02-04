de75d9673498e64bcac4f0cfa97d5549a374785f
monstaro <monstaro32@gmail.com>
2020-02-04 15:39:49 -0700

    Comments out code

--- test/HydrationProfile-test.js
+++ test/HydrationProfile-test.js
@@ -1,18 +1,18 @@
-const chai = require('chai');
-const expect = chai.expect;
+// const chai = require('chai');
+// const expect = chai.expect;
 
-const Hydration = require ('../src/HydrationProfile.js')
-const User = require ('../src/User.js')
+// const Hydration = require ('../src/HydrationProfile.js')
+// const User = require ('../src/User.js')
 
-let user;
-let hydration;
+// let user;
+// let hydration;
 
-describe('Hydration', function() {
-  beforeEach(() => {
-    user = new User(userData)
-  })
-  it('should be a function', function() {
-    expect(Hydration).to.be.a('function');
+// describe('Hydration', function() {
+//   beforeEach(() => {
+//     user = new User(userData)
+//   })
+//   it('should be a function', function() {
+//     expect(Hydration).to.be.a('function');
 
-  });
-})
+//   });
+// })
