41d23ec01c612bedbf20361cb1c39c9cf4d90d07
monstaro <monstaro32@gmail.com>
2020-02-04 15:14:23 -0700

    Initial commit

---
+++ data/sample.js
@@ -0,0 +1,283 @@
+
+
+
+
+const sampleHydration = [
+  {
+    "userID": 1,
+    "date": "2019/06/15",
+    "numOunces": 37
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/15",
+    "numOunces": 75
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/15",
+    "numOunces": 47
+  },
+  {
+    "userID": 4,
+    "date": "2019/06/15",
+    "numOunces": 85
+  },
+  {
+    "userID": 5,
+    "date": "2019/06/15",
+    "numOunces": 42
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/16",
+    "numOunces": 69
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/16",
+    "numOunces": 91
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/16",
+    "numOunces": 99
+  },
+  {
+    "userID": 4,
+    "date": "2019/06/16",
+    "numOunces": 95
+  },
+  {
+    "userID": 5,
+    "date": "2019/06/16",
+    "numOunces": 79
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/17",
+    "numOunces": 96
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/17",
+    "numOunces": 96
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/17",
+    "numOunces": 28
+  },
+  {
+    "userID": 4,
+    "date": "2019/06/17",
+    "numOunces": 82
+  },
+  {
+    "userID": 5,
+    "date": "2019/06/17",
+    "numOunces": 99
+  }
+]
+
+
+const sampleSleep = [
+  {
+    "userID": 1,
+    "date": "2019/06/15",
+    "hoursSlept": 6.1,
+    "sleepQuality": 2.2
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/15",
+    "hoursSlept": 7,
+    "sleepQuality": 4.7
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/15",
+    "hoursSlept": 10.8,
+    "sleepQuality": 4.7
+  },
+  {
+    "userID": 4,
+    "date": "2019/06/15",
+    "hoursSlept": 5.4,
+    "sleepQuality": 3
+  },
+  {
+    "userID": 5,
+    "date": "2019/06/15",
+    "hoursSlept": 4.1,
+    "sleepQuality": 3.6
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/16",
+    "hoursSlept": 4.1,
+    "sleepQuality": 3.8
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/16",
+    "hoursSlept": 7.5,
+    "sleepQuality": 3.8
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/16",
+    "hoursSlept": 10.7,
+    "sleepQuality": 3.4
+  },
+  {
+    "userID": 4,
+    "date": "2019/06/16",
+    "hoursSlept": 8.3,
+    "sleepQuality": 4.5
+  },
+  {
+    "userID": 5,
+    "date": "2019/06/16",
+    "hoursSlept": 7.4,
+    "sleepQuality": 2.4
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/17",
+    "hoursSlept": 8,
+    "sleepQuality": 2.6
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/17",
+    "hoursSlept": 5.7,
+    "sleepQuality": 3
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/17",
+    "hoursSlept": 5.3,
+    "sleepQuality": 4.9
+  },
+  {
+    "userID": 4,
+    "date": "2019/06/17",
+    "hoursSlept": 5.7,
+    "sleepQuality": 1.1
+  },
+  {
+    "userID": 5,
+    "date": "2019/06/17",
+    "hoursSlept": 10.5,
+    "sleepQuality": 3.7
+  }
+]
+
+const sampleActivity = [
+  {
+    "userID": 1,
+    "date": "2019/06/15",
+    "numSteps": 3577,
+    "minutesActive": 140,
+    "flightsOfStairs": 16
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/15",
+    "numSteps": 4294,
+    "minutesActive": 138,
+    "flightsOfStairs": 10
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/15",
+    "numSteps": 7402,
+    "minutesActive": 116,
+    "flightsOfStairs": 33
+  },
+  {
+    "userID": 4,
+    "date": "2019/06/15",
+    "numSteps": 3486,
+    "minutesActive": 114,
+    "flightsOfStairs": 32
+  },
+  {
+    "userID": 5,
+    "date": "2019/06/15",
+    "numSteps": 11374,
+    "minutesActive": 213,
+    "flightsOfStairs": 13
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/16",
+    "numSteps": 6637,
+    "minutesActive": 175,
+    "flightsOfStairs": 36
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/16",
+    "numSteps": 4112,
+    "minutesActive": 220,
+    "flightsOfStairs": 37
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/16",
+    "numSteps": 12304,
+    "minutesActive": 152,
+    "flightsOfStairs": 8
+  },
+  {
+    "userID": 4,
+    "date": "2019/06/16",
+    "numSteps": 10689,
+    "minutesActive": 204,
+    "flightsOfStairs": 10
+  },
+  {
+    "userID": 5,
+    "date": "2019/06/16",
+    "numSteps": 9350,
+    "minutesActive": 167,
+    "flightsOfStairs": 38
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/17",
+    "numSteps": 14329,
+    "minutesActive": 168,
+    "flightsOfStairs": 18
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/17",
+    "numSteps": 13750,
+    "minutesActive": 65,
+    "flightsOfStairs": 4
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/17",
+    "numSteps": 4547,
+    "minutesActive": 97,
+    "flightsOfStairs": 5
+  },
+  {
+    "userID": 4,
+    "date": "2019/06/17",
+    "numSteps": 8160,
+    "minutesActive": 72,
+    "flightsOfStairs": 25
+  },
+  {
+    "userID": 5,
+    "date": "2019/06/17",
+    "numSteps": 13120,
+    "minutesActive": 151,
+    "flightsOfStairs": 25
+  }
+]
