dcc294ac5d135bc6a3556e398330897d9b8648a3
monstaro <monstaro32@gmail.com>
2020-02-04 18:37:57 -0700

    Writes out methods through iteration 2

--- src/HydrationProfile.js
+++ src/HydrationProfile.js
@@ -1,20 +1,38 @@
+const sampleHydration = require ('../data/sample-hydration.js')
+const sampleUsers = require('../data/sample-users.js')
+
+// const hydrationData = require('../data/hydration.js')
+
+
 class HydrationProfile {
-  constructor(udserID) {
-//entries
+  constructor(userid) {
+    this.id = userid
   }
 
   calculateAllTimeOzAvg() {
-// Create an array of all of the ounces and reduce that to calculate avg
+    let usersOunces = sampleHydration.filter(drinker => drinker.userID === this.id);
+
+    return Math.floor(usersOunces.reduce((acc, cur) => {
+      acc += cur.numOunces;
+      return acc
+    }, 0) / usersOunces.length)
   }
 
   findOzConsumed(date) {
-// Filter entries array for specific date
-// Reduce those ounces to one return value
+    // Filter entries array for specific date
+    // Reduce those ounces to one return value
+
+    return sampleHydration.filter(drinker => drinker.userID === this.id).find(drinker => drinker.date === date).numOunces
   }
 
   calculateWeekAvg(startDate) {
-// Filter for date startDate < x < startDate + 7 (a week later)
-// Reduce to get avg
+    // Filter for date startDate < x < startDate + 7 (a week later)
+    // Reduce to get avg
+    let matchId = sampleHydration.filter(drinker => drinker.userID === this.id);
+    return Math.floor(matchId.reduce((acc, cur) => {
+      acc += cur.numOunces
+      return acc
+    }, 0) / matchId.length)
   }
 }
 
