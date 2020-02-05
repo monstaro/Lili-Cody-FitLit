8f207a0a01a27014922cb94e6f9d4bf2c973f9d4
Lili Manrique <liliana.manrique1@gmail.com>
2020-02-05 15:18:20 -0700

    Delete comments

--- src/HydrationProfile.js
+++ src/HydrationProfile.js
@@ -1,9 +1,3 @@
-// const sampleHydration = require ('../data/sample-hydration.js')
-// const sampleUsers = require('../data/sample-users.js')
-
-// const hydrationData = require('../data/hydration.js')
-
-
 class HydrationProfile {
   constructor(userid, data) {
     this.id = userid
@@ -11,7 +5,6 @@ class HydrationProfile {
   }
 
   calculateAllTimeOzAvg() {
-    // let usersOunces = sampleHydration.filter(drinker => drinker.userID === this.id);
     return Math.floor(this.entries.reduce((acc, cur) => {
       acc += cur.numOunces;
       return acc
@@ -19,9 +12,6 @@ class HydrationProfile {
   }
 
   findOzConsumed(date) {
-    // Filter entries array for specific date
-    // Reduce those ounces to one return value
-
     return this.entries.find(drinker => drinker.date === date).numOunces
   }
 
