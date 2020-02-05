5f9d00486a8b5a5e00c525adc0cfc3d857ad1b5a
monstaro <monstaro32@gmail.com>
2020-02-05 16:25:53 -0700

    Moves methods to summary class

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -19,26 +19,6 @@ class UserRepository {
     return (average / this.data.length)
   }
 
-  findAvgSleepQuality() {
-
-  }
-
-  findBestQualitySleepers(startDate) {
-    // Filter by dates (startDate + next 7 days)
-    // Create an array of objects for all sleep averages for that week using reduce {userId: , sleepQualityAvg: }
-    // Filter again for sleepQualityAvg > 3
-
-  }
-
-  findWorstQualitySleepers(startDate) {
-    // We added this method
-    // Potentially, time permitting, we could give these people sleep suggestions/docs in their areas
-  }
-
-  findLongestSleeper(date) {
-    // Use reduce and Math.max to find the highest sleep number
-    // Use filter to find the users who hit that sleep number
-  }
 
   findAvgStairsClimbed(date) {
 
