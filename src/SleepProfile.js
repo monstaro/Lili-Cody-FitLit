217c242eb3359e0f153946887a94e1145a6b099b
Lili Manrique <liliana.manrique1@gmail.com>
2020-02-04 17:59:59 -0700

    Delete unnecssary import and fix data references

--- src/SleepProfile.js
+++ src/SleepProfile.js
@@ -1,16 +1,19 @@
-
-
 class SleepProfile {
-  constructor(userID) {
-    this.entries =
+  constructor(userID, data) {
+    this.entries = data.filter(user => user.userID === userID)
   }
 
   calculateAvgHoursAllTime() {
-
+    const totalSleep = this.entries.reduce((acc, entry) => {
+      acc += entry.hoursSlept;
+      return acc;
+    }, 0);
+    const avg = totalSleep / this.entries.length;
+    return Math.round(avg * 10) / 10;
   }
 
   calculateAvgHoursWeek(startDate) {
-
+    
   }
 
   calculateSleepQualityAllTime() {
