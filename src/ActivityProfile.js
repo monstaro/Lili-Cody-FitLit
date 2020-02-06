4f04409a2e6a990d78f7fa709d6c1dc092f5ba76
monstaro <monstaro32@gmail.com>
2020-02-05 22:48:25 -0700

    Adds function to find last entry

--- src/ActivityProfile.js
+++ src/ActivityProfile.js
@@ -3,18 +3,22 @@ class ActivityProfile {
     this.user = user
     this.entries = data.filter(user => user.userID === this.user.id)
   }
-
+  findLastEntry() {
+    return this.entries[this.entries.length - 1].date
+  }
   findMilesWalked(date) {
     let walkDay = this.entries.find(entry => entry.date === date);
     let totalFt = this.user.strideLength * walkDay.numSteps;
     let numMiles = totalFt / 5280;
     return Math.round((numMiles + Number.EPSILON) * 100) / 100
   }
-
-  findMinutesActive(date) {
-   return this.entries.find(entry => entry.date === date).minutesActive
+  findStepsToday() {
+    console.log(this.entries)
+    return this.entries.find(entry => entry.date === this.findLastEntry()).numSteps
   }
-
+  findMinutesActive(date) {
+    return this.entries.find(entry => entry.date === date).minutesActive
+  }
   findAvgMinActiveWeek(startDate) {
     
   }
@@ -24,21 +28,15 @@ class ActivityProfile {
   }
 
   findDaysGoalExceeded() {
-// Filter entries by this.numSteps > user.stepGoal (get goal objects)
-// Map goal objects to return a dates array
+    // Filter entries by this.numSteps > user.stepGoal (get goal objects)
+    // Map goal objects to return a dates array
   }
 
   findStairRecord() {
-// Use reduce and Math.max to find the highest stair count
-// Use find to return the object of that activity
-// Return the activity.date and highest stair count
+    // Use reduce and Math.max to find the highest stair count
+    // Use find to return the object of that activity
+    // Return the activity.date and highest stair count
   }
-
-  findLastEntry() {
-    return this.entries[this.entries.length - 1].numOunces;
-  }
-
-  
 }
 
 if (typeof module !== 'undefined') {
